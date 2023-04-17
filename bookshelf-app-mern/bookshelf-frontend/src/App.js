546c2feab0bcc97e6449e2184504919580726907
Giselle Manzano <giselleemiliamanzanoo@gmail.com>
2023-04-17 13:01:19 -0600

    creating header component

--- bookshelf-app-mern/bookshelf-frontend/src/App.js
+++ bookshelf-app-mern/bookshelf-frontend/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return (
+ <div className="app">
+ <div className="app__header">
+ <img className="app__headerImage" src="logo192.png" alt="Header" />
+ </div>
+ </div>
+ );
 }
-
 export default App;
