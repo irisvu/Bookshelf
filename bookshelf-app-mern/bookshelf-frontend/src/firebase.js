ac8ba2cd5f32e393b15b8a1da023371f20aeeab6
Giselle Manzano <giselleemiliamanzanoo@gmail.com>
2023-04-17 11:42:02 -0600

    firebase set up

---
+++ bookshelf-app-mern/bookshelf-frontend/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCuQaVDuozggNHUlyzSZXXHfqaFHWvSE7A",
+  authDomain: "bookshelf-app-mern.firebaseapp.com",
+  projectId: "bookshelf-app-mern",
+  storageBucket: "bookshelf-app-mern.appspot.com",
+  messagingSenderId: "490702079298",
+  appId: "1:490702079298:web:29a0b1c1058eea809c74db"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
