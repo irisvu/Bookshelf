44b3f0688474c6fe4bb182377c504715fb981eb8
Giselle Manzano <giselleemiliamanzanoo@gmail.com>
2023-04-17 13:40:43 -0600

    Post Components

---
+++ bookshelf-app-mern/bookshelf-frontend/src/components/Post.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Post.css'
+const Post = () => {
+ return (
+ <div className="post">
+ <h3>TWD</h3>
+ <img className="post__image" src="https://www.techlifediary.
+com/wp-content/uploads/2020/06/react-js.png" alt="React" />
+ <h4 className="post__text"><strong>thewebdev</strong>�Build a 
+Messaging app with MERN (MongoDB, Express, React JS, Node JS) 
+�</h4>
+ </div>
+ )
+}
+
+export default Post
