047a38942a4770d844122b901602363aab5e05ff
Giselle Manzano <giselleemiliamanzanoo@gmail.com>
2023-04-17 13:44:42 -0600

    Photo and Brief Description

--- bookshelf-app-mern/bookshelf-frontend/src/App.js
+++ bookshelf-app-mern/bookshelf-frontend/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Post from './components/Post';
 function App() {
  return (
  <div className="app">
  <div className="app__header">
  <img className="app__headerImage" src="logo192.png" alt="Header" />
  </div>
+ <Post />
+ <Post />
+ <Post />
  </div>
  );
 }
