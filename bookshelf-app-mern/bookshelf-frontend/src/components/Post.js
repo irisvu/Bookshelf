a38ac18bbb0888de55d85c92014c2386696e5553
Giselle Manzano <giselleemiliamanzanoo@gmail.com>
2023-04-17 21:32:52 -0600

    avatar

--- bookshelf-app-mern/bookshelf-frontend/src/components/Post.js
+++ bookshelf-app-mern/bookshelf-frontend/src/components/Post.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import './Post.css'
+import { Avatar } from '@material-ui/core'
 const Post = () => {
  return (
  <div className="post">
+    <div className="post__header">
+    <Avatar
+ className="post__avatar"
+ alt="TWD"
+ src="/static/images/avatar/1.jpg"
+ />
+    
  <h3>TWD</h3>
+ </div>
  <img className="post__image" src="https://www.techlifediary.
 com/wp-content/uploads/2020/06/react-js.png" alt="React" />
  <h4 className="post__text"><strong>thewebdev</strong>�Build a 
