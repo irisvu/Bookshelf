40a89e0a53c3828454531033800f957ca5d2bc72
Giselle Manzano <giselleemiliamanzanoo@gmail.com>
2023-04-17 22:28:20 -0600

    making components Dynamic

--- bookshelf-app-mern/bookshelf-frontend/src/App.js
+++ bookshelf-app-mern/bookshelf-frontend/src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
 import Post from './components/Post';
+import React, { useEffect, useState } from 'react';
+
 function App() {
+
+    const [posts, setPosts] = useState([
+        {
+        username: "TWD",
+        caption: "�Build a Messaging app with MERN Stack�",
+        imageUrl: "https://www.techlifediary.com/wp-content/uploads/2020/06/react-js.png"
+        },
+        {
+        username: "nabendu82",
+        caption: "Such a beautiful world",
+        imageUrl: "https://quotefancy.com/media/wallpaper/3840x2160/126631-Charles-Dickens-Quote-And-a-beautiful-world-you-live-in-when-it-is.jpg"
+        },
+        
+        ])
+    
  return (
  <div className="app">
  <div className="app__header">
  <img className="app__headerImage" src="logo192.png" alt="Header" />
  </div>
+ {posts.map(post => (
+ <Post username={post.username} caption={post.caption} 
+imageUrl={post.imageUrl} />
+ ))}
  <Post />
  <Post />
  <Post />
