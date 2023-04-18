114e21e909555011581cb8760700b70a5bb18605
Giselle Manzano <giselleemiliamanzanoo@gmail.com>
2023-04-17 22:28:38 -0600

    making components dyanmic

--- bookshelf-app-mern/bookshelf-frontend/src/components/Post.js
+++ bookshelf-app-mern/bookshelf-frontend/src/components/Post.js
@@ -1,23 +1,21 @@
 import React from 'react'
 import './Post.css'
 import  Avatar  from '@mui/material/Avatar';
-const Post = () => {
+const Post = ({ username, caption, imageUrl }) => {
  return (
  <div className="post">
     <div className="post__header">
     <Avatar
  className="post__avatar"
- alt="giselle"
+ alt= {username}
  src="/static/images/avatar/1.jpg"
  />
     
- <h3>TWD</h3>
+ <h3>{username}</h3>
  </div>
- <img className="post__image" src="https://www.techlifediary.
-com/wp-content/uploads/2020/06/react-js.png" alt="React" />
- <h4 className="post__text"><strong>thewebdev</strong>�Build a 
-Messaging app with MERN (MongoDB, Express, React JS, Node JS) 
-�</h4>
+ <img className="post__image" src= {imageUrl} alt="React" />
+ <h4 className="post__text"><strong>{username}</strong>{caption} 
+</h4>
  </div>
  )
 }
