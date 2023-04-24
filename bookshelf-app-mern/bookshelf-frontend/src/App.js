14baf0752be740d0cd526b559758b1cceb709900
Giselle Manzano <giselleemiliamanzanoo@gmail.com>
2023-04-24 14:43:34 -0600

    up

--- bookshelf-app-mern/bookshelf-frontend/src/App.js
+++ bookshelf-app-mern/bookshelf-frontend/src/App.js
@@ -1,8 +1,43 @@
 import './App.css';
 import Post from './components/Post';
 import React, { useEffect, useState } from 'react';
+import { styled } from '@mui/material/styles';
+import Modal from '@mui/material/Modal';
+import { Button, Input } from '@mui/material';
+
+
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = styled((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[16],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+
+
+
+
 
 function App() {
+
+    const classes = useStyles();
+    const [modalStyle] = React.useState(getModalStyle);
+    const [open, setOpen] = useState(false)
 
     const [posts, setPosts] = useState([
         {
@@ -20,7 +55,13 @@ function App() {
     
  return (
  <div className="app">
+     <Modal open={open} onClose={() => setOpen(false)}>
+    <div style={modalStyle} className={classes.paper}>
+     <h2>Modal Code</h2>
+    </div>
+    </Modal>
  <div className="app__header">
+    <Button onClick={() => setOpen(true)}>Sign Up</Button>
  <img className="app__headerImage" src="logo192.png" alt="Header" />
  </div>
  {posts.map(post => (
