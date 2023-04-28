b6bfdae0c7e28d2db03921e39190d80a7b689369
Giselle Manzano <giselleemiliamanzanoo@gmail.com>
2023-04-28 17:25:55 -0600

    upload

--- bookshelf-app-mern/bookshelf-frontend/src/components/ImageUpload.js
+++ bookshelf-app-mern/bookshelf-frontend/src/components/ImageUpload.js
@@ -22,11 +22,39 @@ const handleChange = e => {
 
 
 const handleUpload = () => {
- 
-   
-  
-  
 
+  const storage= getStorage();
+
+  const uploadTask = storage.ref(`images/${image.name}`).put(image);
+ uploadTask.on(
+ "state_changed",
+ (snapshot) => {
+ const progress = Math.round(
+ (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+ );
+ setProgress(progress);
+ },(error) => {
+  console.log(error);
+  },
+  () => {
+  storage
+  .ref("images")
+  .child(image.name)
+  .getDownloadURL()
+  .then((url) => {
+  setUrl(url);
+  axios.post('/upload', {
+  caption: caption,
+ user: username,
+ image: url
+  })
+  setProgress(0);
+  setCaption("");
+  setImage(null);
+  })
+  }
+ );
+  
 }
  return (
  <div className='imageUpload'>
