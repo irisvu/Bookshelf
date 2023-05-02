437ff6fec3e2f1db1dff9a9a2eebb71a662e5527
Giselle Manzano <giselleemiliamanzanoo@gmail.com>
2023-05-01 19:07:54 -0600

    up

--- bookshelf-app-mern/bookshelf-frontend/src/axios.js
+++ bookshelf-app-mern/bookshelf-frontend/src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const instance = axios.create({
- baseURL: "https://bookshelfbackend.onrender.com/"
+ baseURL: "https://bookshelfbackend.onrender.com"
 })
 export default instance
  
